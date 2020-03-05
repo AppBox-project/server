e89ab5d00148861683dbc8f56a49a6c1de9f48ec
Vic van Cooten <duveaux1@gmail.com>
2020-03-05 15:14:43 +0100

    Save remote field dependencies for formulas

--- src/Utils/Actions/Formulas.ts
+++ src/Utils/Actions/Formulas.ts
@@ -1,4 +1,5 @@
 import f from "../Functions";
+import { map } from "lodash";
 
 export default [
   // -->
@@ -26,13 +27,92 @@ export default [
           key: args.context
         });
 
-        args.dependencies.map(dep => {
-          const currentDep = currentObj.fields[dep].dependencyFor || [];
-          if (!currentDep.includes(args.fieldId)) {
-            currentDep.push(args.fieldId);
+        // Map dependencies
+        // All fields that this forula depend on will get a tag that they are a dependency.
+        // This will cause the formulafield to be updated once a dependency is updated
+        args.dependencies.map(async dep => {
+          if (dep.match("_r")) {
+            // Relationship dep
+            if (dep.match("[.]")) {
+              // Remote relationship dep
+              const remoteModels = { [args.context]: currentObj };
+              const fieldId = `${args.context}.${args.fieldId}`;
+              console.log(`Logging remote relationship ${dep} for ${fieldId}`);
+              const path = dep.split(".");
+              let modelName = args.context; // First model is always the context for the formula
+              for (let x = 0; x < path.length; x++) {
+                if (!remoteModels[modelName]) {
+                  console.log(`Remotemodels containsn't ${modelName}`); // This shouldn't happen since we scan our models ahead of time
+                }
 
-            currentObj.fields[dep].dependencyFor = currentDep;
-            currentObj.markModified("fields");
+                if (path[x].match("_r")) {
+                  const pathPart = path[x].substr(0, path[x].length - 2);
+
+                  // Find next model
+                  const nextModelId =
+                    remoteModels[modelName].fields[pathPart].typeArgs
+                      .relationshipTo;
+                  if (!remoteModels[nextModelId]) {
+                    remoteModels[
+                      nextModelId
+                    ] = await models.objects.model.findOne({
+                      key: nextModelId
+                    });
+                  }
+
+                  // Now update the current model
+                  const currentDep =
+                    remoteModels[modelName].fields[pathPart].dependencyFor ||
+                    [];
+                  if (!currentDep.includes(fieldId)) {
+                    currentDep.push(fieldId);
+                  }
+                  remoteModels[modelName].fields[
+                    pathPart
+                  ].dependencyFor = currentDep;
+
+                  modelName = nextModelId;
+                } else {
+                  const currentDep =
+                    remoteModels[modelName].fields[path[x]].dependencyFor || [];
+                  if (!currentDep.includes(fieldId)) {
+                    currentDep.push(fieldId);
+                  }
+                  remoteModels[modelName].fields[
+                    path[x]
+                  ].dependencyFor = currentDep;
+                }
+              }
+
+              // Now that we've updated multiple models with their field dependencies, let's save these new models
+              map(remoteModels, (newModel, key) => {
+                if (key !== args.context) {
+                  // Local dependencies set elsewhere
+                  newModel.markModified("fields");
+                  newModel.save();
+                }
+              });
+            } else {
+              // Locale relationship dep
+              const newDep = dep.replace("_r", "");
+
+              const currentDep = currentObj.fields[newDep].dependencyFor || [];
+              if (!currentDep.includes(args.fieldId)) {
+                currentDep.push(args.fieldId);
+
+                currentObj.fields[newDep].dependencyFor = currentDep;
+                currentObj.markModified("fields");
+              }
+            }
+          } else {
+            // Local dep
+            const currentDep = currentObj.fields[dep].dependencyFor || [];
+            if (!currentDep.includes(args.fieldId)) {
+              currentDep.push(args.fieldId);
+
+              currentObj.fields[dep].dependencyFor = currentDep;
+              currentObj.markModified("fields");
+            }
           }
         });
 
