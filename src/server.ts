d01f16f7956bd06789b9d949376a22c71291915c
Duveaux <duveaux1@gmail.com>
2020-04-17 16:56:11 +0200

    Untested: dynamic routing

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ require("./Utils/Models/AppPermissions");
 const app = express();
 app.set("port", config.port);
 app.use("/public", express.static("../../Files/Public"));
-app.use(express.static("../Client/build"));
+app.use('/*',express.static("../Client/build"));
 let http = require("http").Server(app);
 let io = require("socket.io")(http);
 
