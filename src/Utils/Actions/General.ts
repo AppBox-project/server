5ecbabe2b0917a111efafc505d0115b0eeb8e939
Vic van Cooten <duveaux1@gmail.com>
2020-04-27 12:55:06 +0200

    Install app command for supervisor

--- src/Utils/Actions/General.ts
+++ src/Utils/Actions/General.ts
@@ -2,6 +2,7 @@ export default [
   {
     key: "updateBox",
     action: async (args, models, socket, socketInfo) => {
+      // Todo: auth check
       const newTask = {
         type: "Box update",
         name: `Update box software`,
@@ -16,6 +17,27 @@ export default [
         objectId: "system-task",
         data: newTask,
       });
+    },
+  },
+  {
+    key: "installApp",
+    action: async (args, models, socket, socketInfo) => {
+      // Todo: auth check
+      const newTask = await models.entries.model.create({
+        objectId: "system-task",
+        data: {
+          type: "App install",
+          name: `Install ${args.appId}`,
+          description: `Triggered manually`,
+          when: "asap",
+          action: "app-install",
+          state: "Installing app",
+          done: false,
+          arguments: { appId: args.appId },
+          progress: 0,
+        },
+      });
+      socket.emit(`receive-${args.requestId}`, newTask._id);
     },
   },
 ];
