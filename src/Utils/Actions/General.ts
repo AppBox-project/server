2ab4eb8c9d940e2d2e28f7126f2b1ca3d49ba98b
Vic van Cooten <duveaux1@gmail.com>
2020-09-08 12:00:27 +0200

    2fa

--- src/Utils/Actions/General.ts
+++ src/Utils/Actions/General.ts
@@ -1,6 +1,6 @@
 import f from "../Functions";
 import { getIndex } from "../Utils/Index";
-import { setUp2FA } from "./ServerActions";
+import { setUp2FA, compareSecretAndToken } from "./ServerActions";
 const fuzzysort = require("fuzzysort");
 
 export default [
@@ -63,12 +63,26 @@ export default [
   {
     key: "requestAction",
     action: async (args, models, socket, socketInfo) => {
+      const requestArguments = args.args;
+
       switch (args.action) {
         case "setUp2FA":
           socket.emit(
             `receive-${args.requestId}`,
-            setUp2FA(args.appName, args.name)
+            setUp2FA(requestArguments.appName, requestArguments.name)
           );
+          break;
+        case "compareSecretAndToken":
+          compareSecretAndToken(
+            requestArguments.secret,
+            requestArguments.token,
+            requestArguments.objectId,
+            models,
+            requestArguments.enabled_field,
+            requestArguments.secret_field,
+            requestArguments.qr_field,
+            requestArguments.qr
+          ).then((response) => socket.emit(response));
           break;
         default:
           console.log("Unknown action");
--- src/Utils/Actions/ServerActions.ts
+++ src/Utils/Actions/ServerActions.ts
@@ -5,3 +5,35 @@ export const setUp2FA = (appName, userName) =>
     name: appName,
     account: userName,
   });
+
+export const compareSecretAndToken = (
+  secret,
+  token,
+  objectId,
+  models,
+  enabled_field,
+  secret_field,
+  qr_field,
+  qr
+) =>
+  new Promise(async (resolve, reject) => {
+    const result = twoFactor.verifyToken(secret, token);
+    if (!result) {
+      resolve("token-wrong");
+    } else if (result.delta === -1) {
+      resolve("too-late");
+    } else if (result.delta === 1) {
+      resolve("too-early");
+    } else {
+      // TODO: needs to be heavily authorized. Otherwise you can just modify other peoples secrets.
+      const object = await models.entries.model.findOne({ _id: objectId });
+      object.data[enabled_field] = true;
+      object.markModified(`data.${enabled_field}`);
+      object.data[secret_field] = secret;
+      object.markModified(`data.${secret_field}`);
+      object.data[qr_field] = qr;
+      object.markModified(`data.${qr_field}`);
+
+      object.save().then(() => resolve("good"));
+    }
+  });
