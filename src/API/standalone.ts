507e1cbcf4254ee68f7d48347183ae758834a3d9
Vic van Cooten <duveaux1@gmail.com>
2020-11-30 13:45:04 +0100

    Fix standalone

--- src/API/standalone.ts
+++ src/API/standalone.ts
@@ -4,14 +4,23 @@ const executeStandaloneApi = async (models, objectId, req, res, next) => {
   // Find configuration in appsettings based on the key we received and return it.
   const secret = sanitizeString(req.params.optional.split("-")[1]);
   const appId = sanitizeString(req.params.optional.split("-")[0]);
+
   const config = await models.appsettings.model.findOne({
     key: `standaloneConfig-${appId}`,
   });
+
   if (config) {
+    console.log(config.value.secret, secret);
+
     if (config.value.secret === secret) {
-      JSON.stringify(config);
+      res.send(JSON.stringify(config));
     } else {
-      JSON.stringify({ success: false, reason: "secret-does-not-match" });
+      res.send(
+        JSON.stringify({
+          success: false,
+          reason: "secret-does-not-match",
+        })
+      );
     }
   } else {
     res.send(JSON.stringify({ success: false, reason: "app-key-not-found" }));
