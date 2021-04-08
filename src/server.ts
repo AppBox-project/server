8050663533919855092a0ad957d2f379d4ef8bbf
Vic van Cooten <duveaux1@gmail.com>
2021-04-08 12:44:25 +0200

    Serve fontawesome

--- src/server.ts
+++ src/server.ts
@@ -191,6 +191,8 @@ Axios.get(`http://${process.env.DBURL || "localhost:27017"}`)
       });
 
       app.use("/static", express.static("../Client/build/static"));
+      app.use("/fontawesome", express.static("../Client/build/fontawesome"));
+
       app.use("/custom-service-worker.js", function (req, res) {
         res.sendFile(`/AppBox/System/Client/build/custom-service-worker.js`);
       });
